6b9f569dd90ea32b269d307fa18e4361b54b2914
elisaaivarone <lisa.sandrade@yahoo.com.br>
2019-11-11 19:35:36 -0200

    create services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Hero from '../components/Hero'
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
-
+import Services from '../components/Services'
+import FeaturedRooms from '../components/FeaturedRooms'
 export default function Home() {
   return (
+    <React.Fragment>
     <Hero>
       <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
         <Link to="/rooms" className="btn-primary">
@@ -12,6 +14,9 @@ export default function Home() {
         </Link>  
       </Banner>
     </Hero>
+    <Services />
+    <FeaturedRooms />
+    </React.Fragment>  
   );
 }
 
