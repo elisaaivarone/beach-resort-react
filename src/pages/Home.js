b22d91064cc14446f50f22374afffb1c711098b3
elisaaivarone <lisa.sandrade@yahoo.com.br>
2019-11-11 17:26:00 -0200

    create components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import Hero from '../components/Hero'
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-    return <div>Hello from home page</div>;
-    
+  return (
+    <Hero>
+      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+        <Link to="/rooms" className="btn-primary">
+          ours rooms  
+        </Link>  
+      </Banner>
+    </Hero>
+  );
 }
+
